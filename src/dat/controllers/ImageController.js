b0d3b939652cbef681894b2ec93e9b885eec1764
Cassie Tarakajian <ctarakajian@gmail.com>
2018-01-22 15:57:52 -0500

    try again with loading supergif

--- src/dat/controllers/ImageController.js
+++ src/dat/controllers/ImageController.js
@@ -41,6 +41,7 @@ class ImageController extends Controller {
     this.__input.type = 'file';
 
     this.__glGif = new SuperGif({ gif: this.__img });
+    this.__glGif.load((err) => console.log(err));
 
     this.initializeValue();
 
@@ -180,11 +181,6 @@ class ImageController extends Controller {
       this.__glGif.load((err) => {
         if (!err) {
           this.__glGif.play();
-          this.setValue({
-            url: url,
-            type: 'gif',
-            domElement: this.__glGif.get_canvas()
-          });
         }
       });
     }
